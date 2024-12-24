ea79ec319ae13566a17507412c12b2e10ea64ad6
titronius <titronius@gmail.com>
2024-12-24 21:42:06 +0300

    rebuild django

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0nh7s^!9#9w#%zzl1fqcfquwns22g8&)#!y!tk2y80fdccvyn@'
+SECRET_KEY = 'django-insecure-5=hnbxgss&nu20rckr25&rv8a(^z!f2fgxt$@j90o&7$odtg!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

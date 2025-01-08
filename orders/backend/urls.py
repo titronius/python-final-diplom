ca6e8a71d20cdb045c62066b821e1228bcbf99fc
titronius <titronius@gmail.com>
2025-01-08 14:22:05 +0300

    add user password reset

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from backend.views import BasketView, ConfirmToken, ContactView,\
 LoginAccount, OrderView, PartnerOrders, PartnerState, PartnerUpdate, ProductInfoView, RegisterAccount
 
+from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
+
 app_name = 'backend'
 
 urlpatterns = [
@@ -11,6 +13,8 @@ urlpatterns = [
     path('user/login', LoginAccount.as_view(), name='user-login'),
     path('user/register', RegisterAccount.as_view(), name='user-register'),
     path('user/register/confirm', ConfirmToken.as_view(), name='user-register-confirm'),
+    path('user/password_reset', reset_password_request_token, name='password-reset'),
+    path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'),
     path('products', ProductInfoView.as_view(), name='shops'),
     path('basket', BasketView.as_view(), name='basket'),
     path('contact', ContactView.as_view(), name='contact'),
